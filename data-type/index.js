d49afb34e2155ce21687b6443d60372265b977d2
landymiles <onwukalamuel@gmail.com>
2022-07-26 15:00:27 +0300

    hi

---
+++ data-type/index.js
@@ -0,0 +1,20 @@
+console.log("hello")
+let nonso ="nonso"+" "+"onwuka"
+console.log(nonso)
+let x =4
+let y =4
+let add =x+y
+let subtract =y-x
+let divide = y/x
+let multiply= y*x
+let equals = x==y
+let notequals= x!=y
+let wm
+
+console.log(wm)
+console .log (notequals)
+console.log(equals)
+console.log(add)
+console.log(subtract)
+console.log(divide)
+console.log (multiply)
